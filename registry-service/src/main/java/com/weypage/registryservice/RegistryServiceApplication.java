476bdd53360cc726c3d3cf4922d03c688da00100
shiweinan <shiweinan@xiangwushuo.com>
2019-03-14 15:57:58 +0800

    增加注册中心

--- registry-service/src/main/java/com/weypage/registryservice/RegistryServiceApplication.java
+++ registry-service/src/main/java/com/weypage/registryservice/RegistryServiceApplication.java
@@ -1,9 +1,11 @@
-package com.weypage.registry;
+package com.weypage.registryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class RegistryServiceApplication {
 
   public static void main(String[] args) {
--- registry-service/src/test/java/com/weypage/registryservice/RegistryServiceApplicationTests.java
+++ registry-service/src/test/java/com/weypage/registryservice/RegistryServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.weypage.registry;
+package com.weypage.registryservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
